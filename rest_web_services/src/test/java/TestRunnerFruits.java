ebef377a5443b35882ed7dee7125a8ce6658a921
Andrey Dmitriev <dmitriev.andrey.vitalyevich@gmail.com>
2019-03-09 15:07:42 +0300

    Rest services module implemented (draft)

---
+++ rest_web_services/src/test/java/TestRunnerFruits.java
@@ -0,0 +1,26 @@
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+@CucumberOptions(
+        // plugin={"pretty"},
+        plugin={"progress"},
+        // dryRun=true,
+        snippets= SnippetType.CAMELCASE,
+        // features={"src/test/resources/tic_tac_toe"},
+        features={"src/test/resources"},
+        //glue={"tic_tac_toe"}
+        glue={"fruit"}
+        //,tags={"~@Ignore"}
+)
+@Test
+public class TestRunnerFruits extends AbstractTestNGCucumberTests  {
+
+    public void testRunner() {
+        Assert.assertEquals(1, 1);
+
+    }
+
+}
---
+++ rest_web_services/src/test/java/fruit/FruiteSteps.java
@@ -0,0 +1,14 @@
+package fruit;
+        import cucumber.api.java.en.*;
+        import cucumber.api.*;
+
+public class FruiteSteps {
+    @Given("^the system knows about the following fruit:$")
+    public void theSystemKnowsAboutTheFollowingFruit(DataTable arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        // For automatic transformation, change DataTable to one of
+        // List<YourType>, List<List<E>>, List<Map<K,V>> or Map<K,V>.
+        // E,K,V must be a scalar (String, Integer, Date, enum etc)
+        throw new PendingException();
+    }
+}
