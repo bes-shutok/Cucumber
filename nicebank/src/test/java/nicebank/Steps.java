92ffcd3cbf74f4d4bc2b47561988ea4a9ca73c61
Andrey Dmitriev <dmitriev.andrey.vitalyevich@gmail.com>
2019-03-23 15:10:51 +0300

    Nicebank. First step passed

---
+++ nicebank/src/test/java/nicebank/Steps.java
@@ -0,0 +1,33 @@
+package nicebank;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.*;
+import nicebank.Money;
+import org.testng.Assert;
+
+public class Steps {
+    class Account {
+        private Money balance = new Money();
+        public void deposit(Money amount) {balance=balance.add(amount);}
+        public Money getBallance(){return balance;}
+    }
+    @Given("^I have deposited \\$(\\d+)\\.(\\d+) in my account$")
+    public void iHaveDeposited$InMyAccount(int dollars, int cents) throws Throwable {
+        Account myAccount = new Account();
+        Money amount = new Money(dollars, cents);
+        myAccount.deposit(amount);
+        Assert.assertEquals(myAccount.getBallance(),amount,"Incorrect account balance - ");
+    }
+
+    @When("^I request \\$(\\d+)$")
+    public void iRequest$(int arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^\\$(\\d+) should be dispensed$")
+    public void $ShouldBeDispensed(int arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
