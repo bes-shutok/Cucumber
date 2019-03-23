3c71b1469b9570fb62d5863a6c68dc4d20585f3f
Andrey Dmitriev <dmitriev.andrey.vitalyevich@gmail.com>
2019-03-23 13:31:37 +0300

    Nicebank. Steps undefined

---
+++ nicebank/src/test/java/TestRunnerNicebank.java
@@ -0,0 +1,20 @@
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+@CucumberOptions(
+        plugin={"pretty", "html:out"},
+        // plugin={"progress"}, dryRun=true,
+        snippets= SnippetType.CAMELCASE
+        //,features={"src/test/resources"}, glue={"fruit"}, tags={"~@Ignore"}
+)
+@Test
+public class TestRunnerNicebank extends AbstractTestNGCucumberTests {
+        public void testRunner() {
+        Assert.assertEquals(1, 1);
+
+    }
+
+}
