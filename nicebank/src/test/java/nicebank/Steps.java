86f5eb8b97918724bab043fed6cdbaec85b2e4a3
Andrey Dmitriev <dmitriev.andrey.vitalyevich@gmail.com>
2019-03-28 13:57:40 +0300

    Nicebank. Third step failing

--- nicebank/src/test/java/nicebank/Steps.java
+++ nicebank/src/test/java/nicebank/Steps.java
@@ -8,28 +8,55 @@ import org.testng.Assert;
 import transforms.MoneyConverter;
 
 public class Steps {
+    class Teller  {
+        public void withdrawFrom(Account account, Money amount) {
+            account.withdraw(amount);
+        }
+    }
+    class CashSlot {
+        public Money getContents() {
+            return new Money(0,0);
+        }
+    }
     class Account {
         private Money balance = new Money();
         public void deposit(Money amount) {balance=balance.add(amount);}
+        public void withdraw(Money amount) {balance=balance.minus(amount);}
         public Money getBallance(){return balance;}
     }
+    class KnowsTheDomain {
+        private Account myAccount;
+        private CashSlot cashSlot;
+
+        public Account getMyAccount() {
+            if (myAccount == null) {myAccount = new Account();}
+            return myAccount;
+        }
+
+        public CashSlot getCashSlot() {
+            if (cashSlot == null) {cashSlot = new CashSlot();}
+            return cashSlot;
+        }
+    }
+    KnowsTheDomain helper;
+    public Steps() {
+        helper = new KnowsTheDomain();
+    }
+
     @Given("^I have deposited (\\$\\d+\\.\\d+) in my account$")
     public void iHaveDeposited$InMyAccount(@Transform(MoneyConverter.class) Money amount) throws Throwable {
-        Account myAccount = new Account();
-        // Money amount = new Money(dollars, cents);
-        myAccount.deposit(amount);
-        Assert.assertEquals(myAccount.getBallance(),amount,"Incorrect account balance - ");
+        helper.getMyAccount().deposit(amount);
+        Assert.assertEquals(helper.getMyAccount().getBallance(),amount,"Incorrect account balance - ");
     }
 
-    @When("^I request \\$(\\d+)$")
-    public void iRequest$(int arg1) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    @When("^I withdraw (\\$\\d+\\.\\d+)$")
+    public void iWithdraw$(@Transform(MoneyConverter.class) Money amount) throws Throwable {
+        Teller teller = new Teller();
+        teller.withdrawFrom(helper.getMyAccount(), amount);
     }
 
-    @Then("^\\$(\\d+) should be dispensed$")
-    public void $ShouldBeDispensed(int arg1) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    @Then("^(\\$\\d+\\.\\d+) should be dispensed$")
+    public void $ShouldBeDispensed(@Transform(MoneyConverter.class) Money amount) throws Throwable {
+        Assert.assertEquals(helper.getCashSlot().getContents(),amount,"Incorrect account balance - ");
     }
 }
